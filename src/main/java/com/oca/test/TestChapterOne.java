693cd7e9cc181202f96a37bfff85bc75fda05f26
Pavel Golovenkov <golovenkovpv@nspk.ru>
2019-03-13 11:22:17 +0300

    Added methods for printing

--- src/main/java/com/oca/test/TestChapterOne.java
+++ src/main/java/com/oca/test/TestChapterOne.java
@@ -3,7 +3,22 @@ package com.oca.test;
 public class TestChapterOne {
 
 	public static void main(String[] args) {
-
+		printWelcome();
+		printArgs(args);
 	}
+
+	private static void printWelcome() {
+		System.out.println("Welcome!");
+	}
+
+	private static void printArgs(String... args) {
+		if (args.length < 2) {
+			return;
+		}
+
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+	}
+
 }
 
