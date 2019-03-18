2c9954a84c595f4b2511d95d8fe502a71da63f9e
Pavel Golovenkov <golovenkovpv@nspk.ru>
2019-03-18 10:43:16 +0300

    Added answers on chapter 1 review questions

--- src/main/java/com/oca/test/chapter1/TestChapterOne.java
+++ src/main/java/com/oca/test/chapter1/TestChapterOne.java
@@ -6,12 +6,47 @@ import java.util.Random;
 
 public class TestChapterOne {
 
+	private String brand;
+	private boolean empty;
+
+	/*
+	* Review questions
+	* 1. A, B, E +
+	* 2. D +
+	* 3. B, D, E +
+	* 4. A, B +
+	* 5. C, D +
+	* 6. E +
+	* 7. A, B +-
+	* 8. B +
+	* 9. A, C, D, E +
+	* 10. E +
+	* 11. C, D +
+	* 12. G +
+	* 13. A, D +
+	* 14. D +
+	* 15. A, E +
+	* 16. B, C, D +
+	* 17. A, E +
+	* 18. C, E +-
+	* 19. B, D +
+	* 20. B, E +
+	* 21. A +
+	* 22. B, C, E, F +-
+	* 23. C, F +-
+	* */
 	public static void main(String[] args) {
 		printWelcome();
 		printArgs(args);
 		printRandom();
 		createClasses();
 		findAnswer(true);
+		printDouble();
+	}
+
+	private static void printDouble(){
+		double amount = 0xE;
+		System.out.println(amount);
 	}
 
 	private static void printWelcome() {
@@ -52,6 +87,11 @@ public class TestChapterOne {
 			answer = 2;
 		}
 		System.out.println(answer);
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
 	}
 }
 
