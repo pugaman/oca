e268ddf8f0cde62fd205eccdc2c5e3f0bb072f98
Pavel Golovenkov <golovenkovpv@nspk.ru>
2019-03-21 13:26:57 +0300

    Added test methods for date and time

--- src/main/java/com/oca/test/chapter3/TestChapterThree.java
+++ src/main/java/com/oca/test/chapter3/TestChapterThree.java
@@ -1,5 +1,9 @@
 package com.oca.test.chapter3;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,6 +20,10 @@ public class TestChapterThree {
 		arrayTypes();
 		arrayStoreException();
 		arrayToList();
+		dateTime();
+		localDate();
+		localTime();
+		localDateTime();
 
 	}
 
@@ -45,7 +53,38 @@ public class TestChapterThree {
 		System.out.println(list.size()); // 2
 		list.set(1, "test"); // [hawk, test]
 		array[0] = "new"; // [new, test]
-		for (String b : array) System.out.print(b + " "); // new test
+		for (String b : array) System.out.println(b + " "); // new test
 //		list.remove(1);
+	}
+
+	private static void dateTime(){
+		System.out.println(LocalDate.now());
+		System.out.println(LocalTime.now());
+		System.out.println(LocalDateTime.now());
+	}
+
+	private static void localDate(){
+		LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 20);
+		LocalDate date2 = LocalDate.of(2015, 1, 20);
+		System.out.println(date1);
+		System.out.println(date2);
+	}
+
+	private static void localTime(){
+		LocalTime time1 = LocalTime.of(6, 15); // hour and minute
+		LocalTime time2 = LocalTime.of(6, 15, 30); // + seconds
+		LocalTime time3 = LocalTime.of(6, 15, 30, 200); // + nanoseconds
+		System.out.println(time1);
+		System.out.println(time2);
+		System.out.println(time3);
+	}
+
+	private static void localDateTime(){
+		LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 20);
+		LocalTime time1 = LocalTime.of(6, 15); // hour and minute
+		LocalDateTime dateTime1 = LocalDateTime.of(2015, Month.JANUARY, 20, 6, 15, 30);
+		LocalDateTime dateTime2 = LocalDateTime.of(date1, time1);
+		System.out.println(dateTime1);
+		System.out.println(dateTime2);
 	}
 }
